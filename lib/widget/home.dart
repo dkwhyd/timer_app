6218f54854b6796d7561cd77c59f68bd623762a0
Diki Wahyudi <dikiwhyd99@gmail.com>
2024-02-25 13:36:02 +0700

    change layout home

--- lib/widget/home.dart
+++ lib/widget/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timer/widget/timer_button.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,24 +11,99 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final double defaultPadding = 5.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Center(
-          child: Text(
-            widget.title,
-            style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Center(
+            child: Text(
+              widget.title,
+              style: const TextStyle(color: Colors.white),
+            ),
           ),
         ),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
-    );
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(defaultPadding),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                    child: TimerButton(
+                        size: 15,
+                        color: const Color(0xff009688),
+                        text: "Work",
+                        onPressed: emptyMethod)),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                    child: TimerButton(
+                        size: 15,
+                        color: const Color(0xff607D8B),
+                        text: "Short Break",
+                        onPressed: emptyMethod)),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                    child: TimerButton(
+                        size: 15,
+                        color: const Color(0xff455A64),
+                        text: "Long Break",
+                        onPressed: emptyMethod)),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Text('hello'),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                  child: TimerButton(
+                    color: Color(0xff212121),
+                    text: 'Stop',
+                    onPressed: emptyMethod,
+                    size: 15,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                    child: TimerButton(
+                  color: Color(0xff009688),
+                  text: 'Restart',
+                  onPressed: emptyMethod,
+                  size: 15,
+                )),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(defaultPadding),
+            ),
+          ],
+        ));
   }
+
+  void emptyMethod() {}
 }
