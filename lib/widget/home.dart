2af3e89b3388f820189faf00d3d018f442f01a2e
Diki Wahyudi <dikiwhyd99@gmail.com>
2024-02-25 13:52:41 +0700

    add CircularPercentIndicator

--- lib/widget/home.dart
+++ lib/widget/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timer/widget/timer_button.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -16,93 +17,105 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Center(
-            child: Text(
-              widget.title,
-              style: const TextStyle(color: Colors.white),
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Center(
+          child: Text(
+            widget.title,
+            style: const TextStyle(color: Colors.white),
           ),
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(defaultPadding),
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final double availableWidth = constraints.maxHeight;
+          return Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(defaultPadding),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Expanded(
+                      child: TimerButton(
+                          size: 15,
+                          color: const Color(0xff009688),
+                          text: "Work",
+                          onPressed: emptyMethod)),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Expanded(
+                      child: TimerButton(
+                          size: 15,
+                          color: const Color(0xff607D8B),
+                          text: "Short Break",
+                          onPressed: emptyMethod)),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Expanded(
+                      child: TimerButton(
+                          size: 15,
+                          color: const Color(0xff455A64),
+                          text: "Long Break",
+                          onPressed: emptyMethod)),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: CircularPercentIndicator(
+                  radius: availableWidth / 4,
+                  lineWidth: 10.0,
+                  percent: 1,
+                  center: Text('30:00'),
+                  progressColor: Color(0xff009688),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-                Expanded(
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Expanded(
                     child: TimerButton(
-                        size: 15,
-                        color: const Color(0xff009688),
-                        text: "Work",
-                        onPressed: emptyMethod)),
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-                Expanded(
-                    child: TimerButton(
-                        size: 15,
-                        color: const Color(0xff607D8B),
-                        text: "Short Break",
-                        onPressed: emptyMethod)),
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-                Expanded(
-                    child: TimerButton(
-                        size: 15,
-                        color: const Color(0xff455A64),
-                        text: "Long Break",
-                        onPressed: emptyMethod)),
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-              ],
-            ),
-            Expanded(
-              child: Text('hello'),
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-                Expanded(
-                  child: TimerButton(
-                    color: Color(0xff212121),
-                    text: 'Stop',
+                      color: Color(0xff212121),
+                      text: 'Stop',
+                      onPressed: emptyMethod,
+                      size: 15,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                  ),
+                  Expanded(
+                      child: TimerButton(
+                    color: Color(0xff009688),
+                    text: 'Restart',
                     onPressed: emptyMethod,
                     size: 15,
+                  )),
+                  Padding(
+                    padding: EdgeInsets.all(defaultPadding),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-                Expanded(
-                    child: TimerButton(
-                  color: Color(0xff009688),
-                  text: 'Restart',
-                  onPressed: emptyMethod,
-                  size: 15,
-                )),
-                Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(defaultPadding),
-            ),
-          ],
-        ));
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(defaultPadding),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 
   void emptyMethod() {}
