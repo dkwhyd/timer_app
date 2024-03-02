950cfca890f1ce698cfaf5eb2bfea31b3e7f5dc2
Diki Wahyudi <dikiwhyd99@gmail.com>
2024-03-02 22:09:40 +0700

    add setting layout to settings screen

--- lib/widget/settings_screen.dart
+++ lib/widget/settings_screen.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:timer/widget/settings.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text(
-            'Settings',
-            style: TextStyle(),
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'Settings',
+              style: TextStyle(),
+            ),
           ),
         ),
-      ),
-      body: Container(
-        child: Text('seting'),
-      ),
-    );
+        body: Settings());
   }
 }
