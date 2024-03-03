3754b3e5c5c26b853c00125622380dac015a22ac
Diki Wahyudi <dikiwhyd99@gmail.com>
2024-03-03 13:59:13 +0700

    add size, setting, & callback,

--- lib/widget/settings_button.dart
+++ lib/widget/settings_button.dart
@@ -3,18 +3,23 @@ import 'package:flutter/material.dart';
 class SettingButton extends StatelessWidget {
   final Color color;
   final String text;
+  final double size;
   final int value;
+  final String setting;
+  final Function callback;
 
-  const SettingButton(this.color, this.text, this.value, {super.key});
+  const SettingButton(this.color, this.text, this.size, this.value,
+      {super.key, required this.setting, required this.callback});
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
-      onPressed: () {},
+      onPressed: () => callback(this.setting, this.value),
+      color: color,
+      minWidth: size,
       child: Text(
-        this.text,
-        style: TextStyle(color: Colors.white),
+        text,
+        style: const TextStyle(color: Colors.white),
       ),
-      color: this.color,
     );
   }
 }
